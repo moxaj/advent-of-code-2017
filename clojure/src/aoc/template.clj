cecce5bb1e151b60e9d57ff1d8f0e8351481d007
moxaj <viktor.magyari92@gmail.com>
2017-12-02 12:16:03 +0100

    Add solution template

---
+++ clojure/src/aoc/template.clj
@@ -0,0 +1,11 @@
+(ns aoc.day-X
+  (:require [clojure.java.io :as io]
+            [clojure.string :as string]))
+
+(def input (slurp (io/resource "aoc/day_X.txt")))
+
+(defn solve [input]
+  "TODO")
+
+(comment
+  (solve input))
